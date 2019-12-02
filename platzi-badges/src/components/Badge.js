ea236525aeb68e606de95e281b2229e57c811ad3
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-02 10:54:20 -0500

    10-Including gravatar

--- platzi-badges/src/components/Badge.js
+++ platzi-badges/src/components/Badge.js
@@ -5,8 +5,17 @@ class Badge extends React.Component {
     render() {
         return (
             <div>
-                <img src={ConfLogo} />
-                <h1>Badge</h1>
+                <div>
+                    <img src={ConfLogo} alt="Logo de la Platzi-Conf"/>
+                </div>
+                <div>
+                    <img src="https://en.gravatar.com/userimage/35531970/01270987868c91ce3a31faa7b4441490.png" alt="Avatar de gravatar"/>
+                    <h1>Juan <br/>Reyes</h1>
+                </div>
+                <div>
+                    <p>Economista y front end developer</p>
+                    <p>@jdreyespaez</p>
+                </div>
             </div>
         )
     }
