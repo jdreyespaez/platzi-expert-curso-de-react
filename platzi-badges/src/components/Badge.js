55e9d523ad5a641eeac29b64161f4eb24ee2acb9
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-03 19:35:04 -0500

    11- Creating styles, as a stylesheet being imported to component

--- platzi-badges/src/components/Badge.js
+++ platzi-badges/src/components/Badge.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import ConfLogo from '../images/badge-header.svg';
+import './styles/Badge.css'
+
 
 class Badge extends React.Component {
     render() {
         return (
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={ConfLogo} alt="Logo de la Platzi-Conf"/>
                 </div>
-                <div>
-                    <img src="https://en.gravatar.com/userimage/35531970/01270987868c91ce3a31faa7b4441490.png" alt="Avatar de gravatar"/>
+                <div className="Badge__section-name">
+                    <img 
+                        className="Badge__avatar" 
+                        src="https://en.gravatar.com/userimage/35531970/01270987868c91ce3a31faa7b4441490.png" 
+                        alt="Avatar de gravatar"
+                    />
                     <h1>Juan <br/>Reyes</h1>
                 </div>
-                <div>
+                <div className="Badge__section-info">
                     <p>Economista y front end developer</p>
                     <p>@jdreyespaez</p>
                 </div>
+                <div className="Badge__footer">#platziConf</div>
             </div>
         )
     }
