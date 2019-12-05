d277946df3320ec71971ccfdf50dfc522e0ce74e
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-05 17:01:37 -0500

    13- Adding Navbar styles

--- platzi-badges/src/components/Navbar.js
+++ platzi-badges/src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import './styles/Navbar.css'
+import logo from '../images/logo.svg'
+
 class Navbar extends React.Component {
     render() {
         return (
-            <div>
-                <h2>Soy el Navbar</h2>
+            <div class="Navbar">
+                <div className="container-fluid">
+                    <a className="Navbar__brand" href="/">
+                        <img className="Navbar__brand-logo" src={logo} alt="logo"/>
+                        <span className="font-weight-light">Platzi</span>
+                        <span className="font-weight-bold">Conf</span>
+                    </a>
+                </div>
             </div>
         )
     }
