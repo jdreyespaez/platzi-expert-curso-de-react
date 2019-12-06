4777bb6b8c2f03ad0ba0cfdcd7daf4b2c0b02d12
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-06 14:58:42 -0500

    13- Including styles to component NewBadge.js

--- platzi-badges/src/components/pages/BadgeNew.js
+++ platzi-badges/src/components/pages/BadgeNew.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+import '../styles/BadgeNew.css'
 
 import header from '../../images/badge-header.svg'
 import Navbar from '../Navbar'
 
-class BadgeNew extends React.Component {
+class BadgeNew extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <div className="BadgeNew_hero">
+                <div className="BadgeNew__hero">
                     <img className="img-fluid" src={header} alt="Logo" />
                 </div>
             </div>
