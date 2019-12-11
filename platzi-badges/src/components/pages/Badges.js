df397f1c9944711aa5aa4b1189dd124bff523ecf
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-11 18:15:19 -0500

    17.1- Styling card

--- platzi-badges/src/components/pages/Badges.js
+++ platzi-badges/src/components/pages/Badges.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
-
 import Navbar from '../Navbar'
+
+import '../styles/Badges.css'
+import ConfLogo from '../../images/badge-header.svg'
 
 class Badges extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <div className="Badges">
+          <div className="Badges__hero">
+            <div className="Badges__container">
+              <img 
+                src={ConfLogo} 
+                alt="Logo de la Platzi Conf" 
+                className="Badges_conf-logo"/>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
