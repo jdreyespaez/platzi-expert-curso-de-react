94b55025783bdd598a20b2f04d1e0189289551be
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-11 17:57:01 -0500

    17.1- Adding the Navbar component to the Badges page

--- platzi-badges/src/components/pages/Badges.js
+++ platzi-badges/src/components/pages/Badges.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
 
+import Navbar from '../Navbar'
+
 class Badges extends Component {
   render() {
-    return 'Badges'
+    return (
+      <div>
+        <Navbar />
+      </div>
+    )
   }
 }
 
