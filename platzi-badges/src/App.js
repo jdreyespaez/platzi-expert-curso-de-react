adc147f3da5ad712a3591c40f4a5f265ee0fabba
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-14 18:09:23 -0500

    20.1- Creating Layout and passing props.children

--- platzi-badges/src/App.js
+++ platzi-badges/src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
+import Layout from './components/Layout'
 import Badges from './components/pages/Badges'
 import BadgeNew from './components/pages/BadgeNew'
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/badges" component={Badges}/>
-        <Route exact path="/badges/new" component={BadgeNew}/>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact path="/badges" component={Badges}/>
+          <Route exact path="/badges/new" component={BadgeNew}/>
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 }
