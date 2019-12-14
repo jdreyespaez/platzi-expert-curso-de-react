e99dcde430f22bf03a551ca1ca633d21af31e6f6
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-13 19:48:31 -0500

    19.2- Configuring BrowserRoute

--- platzi-badges/src/App.js
+++ platzi-badges/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import {BrowserRouter, Route} from 'react-router-dom'
+
 import Badges from './components/pages/Badges'
+import BadgeNew from './components/pages/BadgeNew'
 
 function App() {
   return (
-    <div className="App">
-      <Badges/>
-    </div>
+    <BrowserRouter>
+      <Route path="/badges" component={Badges}/>
+      <Route path="/badges/new" component={BadgeNew}/>
+    </BrowserRouter>
   );
 }
 
