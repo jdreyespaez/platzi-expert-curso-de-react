0083c795bcfe3aa972ca58cd2e874d7bbfb10e1e
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-16 22:54:19 -0500

    20.1- Passing props to children

--- platzi-badges/src/components/Layout.js
+++ platzi-badges/src/components/Layout.js
@@ -2,7 +2,12 @@ import React from 'react';
 import Navbar from './Navbar'
 
 function Layout(props) {
-  return props.children
+  return (
+    <div>
+      <Navbar />
+      {props.children}
+    </div>
+  )
 }
 
 export default Layout;
